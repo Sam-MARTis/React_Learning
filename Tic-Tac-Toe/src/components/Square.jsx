400c133b3c393b580c3a0076f8180291444c0a95
Sam-MARTis <samanthmartis@gmail.com>
2024-02-15 22:16:11 +0530

    made smaller component square

---
+++ Tic-Tac-Toe/src/components/Square.jsx
@@ -0,0 +1,16 @@
+import "./styles/Square.css";
+
+const Square = ({ value }) => {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+    </>
+  );
+};
+
+export default Square;
