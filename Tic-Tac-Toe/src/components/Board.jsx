f7845112bb208379ae5a31af3a02d214e64d9ab8
Sam-MARTis <samanthmartis@gmail.com>
2024-02-16 21:44:36 +0530

    Stops when won

--- Tic-Tac-Toe/src/components/Board.jsx
+++ Tic-Tac-Toe/src/components/Board.jsx
@@ -5,16 +5,39 @@ const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   const [isX, setIsX] = useState(true);
 
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   const handleClick = (i) => {
     const newState = state.slice();
-    if (newState[i]) return;
+    if (newState[i] || calculateWinner(newState)) return;
+
     if (isX) {
       console.log("clicked ");
-      
+
       newState[i] = "X";
-      
-    }
-    else {
+    } else {
       console.log("clicked ");
       newState[i] = "O";
     }
@@ -25,19 +48,64 @@ const Board = () => {
   return (
     <>
       <div className="board-row">
-        <Square value={state[0]} onClick={() => {handleClick(0)}} />
-        <Square value={state[1]} onClick={() => {handleClick(1)}} />
-        <Square value={state[2]} onClick={() => {handleClick(2)}} />
+        <Square
+          value={state[0]}
+          onClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={state[1]}
+          onClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={state[2]}
+          onClick={() => {
+            handleClick(2);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square value={state[3]} onClick={() => {handleClick(3)}} />
-        <Square value={state[4]} onClick={() => {handleClick(4)}} />
-        <Square value={state[5]} onClick={() => {handleClick(5)}} />
+        <Square
+          value={state[3]}
+          onClick={() => {
+            handleClick(3);
+          }}
+        />
+        <Square
+          value={state[4]}
+          onClick={() => {
+            handleClick(4);
+          }}
+        />
+        <Square
+          value={state[5]}
+          onClick={() => {
+            handleClick(5);
+          }}
+        />
       </div>
       <div className="board-row">
-        <Square value={state[6]} onClick={() => {handleClick(6)}} />
-        <Square value={state[7]} onClick={() => {handleClick(7)}} />
-        <Square value={state[8]} onClick={() => {handleClick(8)}} />
+        <Square
+          value={state[6]}
+          onClick={() => {
+            handleClick(6);
+          }}
+        />
+        <Square
+          value={state[7]}
+          onClick={() => {
+            handleClick(7);
+          }}
+        />
+        <Square
+          value={state[8]}
+          onClick={() => {
+            handleClick(8);
+          }}
+        />
       </div>
     </>
   );
