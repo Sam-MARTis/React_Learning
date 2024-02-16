85ba85a3ceb5fe684df47fd18eeee5d990a8faee
Sam-MARTis <samanthmartis@gmail.com>
2024-02-16 21:23:19 +0530

    Added single x changer when pushed

--- Tic-Tac-Toe/src/components/Board.jsx
+++ Tic-Tac-Toe/src/components/Board.jsx
@@ -1,22 +1,30 @@
 import Square from "./Square";
+import { useState } from "react";
 import "./styles/Board.css";
 const Board = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+  const handleClick = () => {
+    console.log("clicked ");
+    const newState = state.slice();
+    newState[0] = "X";
+    setState(newState);
+  };
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square value={state[0]} onClick={handleClick} />
+        <Square value={state[1]} onClick={handleClick} />
+        <Square value={state[2]} onClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square value={state[3]} onClick={handleClick} />
+        <Square value={state[4]} onClick={handleClick} />
+        <Square value={state[5]} onClick={handleClick} />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square value={state[6]} onClick={handleClick} />
+        <Square value={state[7]} onClick={handleClick} />
+        <Square value={state[8]} onClick={handleClick} />
       </div>
     </>
   );
--- Tic-Tac-Toe/src/components/Square.jsx
+++ Tic-Tac-Toe/src/components/Square.jsx
@@ -1,15 +1,20 @@
 import "./styles/Square.css";
 import { useState } from "react";
-const Square = () => {
+const Square = ({value, onClick}) => {
 
-    const [value, setValue] = useState(null);
-    const handleClick = () => {
-      setValue("X");
-    console.log("clicked");
-  };
+    // const [value, setValue] = useState(null);
+//     const handleClick = () => {
+//       setValue("X");
+//     console.log("clicked");
+  //   };
+    const doHandleClick = () => {
+        if (value === null) {
+            onClick();
+        }
+    };
   return (
     <>
-      <button className="square" onClick={handleClick}>
+      <button className="square" onClick={doHandleClick}>
         {value}
       </button>
     </>
